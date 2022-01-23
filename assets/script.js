f2cc28393e4098faa15df194b5fe6c67543fbcc3
aditore <anthonyditore@gmail.com>
2022-01-23 12:35:21 -0800

    fixed some styling issues, added local storage saver

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,18 @@ var showCurrentDay = $('#currentDay');
 var timeBlock = $('.container')
 
 /* function */
+//begin function to save to local storage
+$(document).ready(function () {
+    //save button click
+    $(".saveBtn").on("click", function () {
+        //attain values of .description in jQuery
+        var inputText = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+        //save to local storage
+        localStorage.setItem(time, inputText);
+    })
+})
+
 
 //show current day (function)
 function displayCurrentDay() {
